9268bb64d27778a7f32ddb024c03147c161bec00
DXF0530 <dmitry.fleish@sbdinc.com>
2020-06-01 09:29:14 +0300

    30 minutes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:  {
+    apiKey: "AIzaSyCB9as_UW7uS00pBsw1VqD9sidBKytV-8k",
+    authDomain: "ng-il-club-2ef3a.firebaseapp.com",
+    databaseURL: "https://ng-il-club-2ef3a.firebaseio.com",
+    projectId: "ng-il-club-2ef3a",
+    storageBucket: "ng-il-club-2ef3a.appspot.com",
+    messagingSenderId: "614209120334",
+    appId: "1:614209120334:web:f1d477e1bde4aa2d8e3b1f"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:  {
+    apiKey: "AIzaSyCB9as_UW7uS00pBsw1VqD9sidBKytV-8k",
+    authDomain: "ng-il-club-2ef3a.firebaseapp.com",
+    databaseURL: "https://ng-il-club-2ef3a.firebaseio.com",
+    projectId: "ng-il-club-2ef3a",
+    storageBucket: "ng-il-club-2ef3a.appspot.com",
+    messagingSenderId: "614209120334",
+    appId: "1:614209120334:web:f1d477e1bde4aa2d8e3b1f"
+  }
 };
 
 /*
