b15b7b3eef992e15e286bb9fb3780e549800e633
DXF0530 <dmitry.fleish@sbdinc.com>
2020-06-07 21:40:36 +0300

    commit-070620202140

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import * as firebase from 'firebase';
 
 interface User {
+  uid: string;
   email: string;
   display_name: string;
   image_url: string;
@@ -24,10 +25,19 @@ export class AuthService {
     private router: Router
   ) {}
 
-  async signInWithGoogle() {
+  async signInWithGoogle(): Promise<void> {
     const provider = new firebase.auth.GoogleAuthProvider();
     const credetionals =  await this.angularFireAuth.signInWithPopup(provider);
-    console.log(credetionals);
+    const userRef: AngularFirestoreDocument<User> = this.angularFirestore.doc(`users/${credetionals.user.uid}`);
+    const userData: User = {
+      uid: credetionals.user.uid,
+      display_name: credetionals.user.displayName,
+      email: credetionals.user.email,
+      image_url: credetionals.user.photoURL,
+      roles: {memeber: true}
+    };
+
+    return userRef.set(userData, {merge: true});
   }
 
   async signOut() {
